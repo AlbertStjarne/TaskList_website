a8382e5a0a122d58d4ec586dbdc20a5cb450fd23
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-17 19:30:24 +0100

    adds js to filter tasks

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear task event
   clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks events
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -59,11 +61,23 @@ function removeTask(e) {
 }
 
 // Clear tasks
-function clearTasks(e) {
+function clearTasks() {
   while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
 }
 
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
 
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
 
